7c3aac7d63029a1b5aefe8354a0757a2eb42de47
uryoya <uryoya.ur@gmail.com>
2016-08-03 15:45:59 +0900

    configファイル読みこむようにした

--- typhoon.py
+++ typhoon.py
@@ -15,11 +15,12 @@ import json
 import db
 import hashlib
 import model
+import config
 
 from tornado.options import define, options
 
 
-define("port", default=8888, help="run on the given port", type=int)
+define("port", default=config.web_server['port'], help="run on the given port", type=int)
 
 class Application(tornado.web.Application):
     """
